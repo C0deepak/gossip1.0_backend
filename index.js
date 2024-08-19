93b971ab6cd3ff3b13ee553687cb565ac36aa896
Deepak <deepakkumarkasyap12@gmail.com>
2024-08-19 15:52:53 +0530

    backend url changes

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const server = app.listen(PORT, console.log(`App is runing @port: ${PORT}`))
 const io = require('socket.io')(server, {
     pingTimeout: 60000,
     cors: {
-        origin: 'http://localhost:3000'
+        origin: 'https://gossip-frontend-sigma.vercel.app'
     }
 })
 
